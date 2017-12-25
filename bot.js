aa7a4d86101313bc5266ca65e7d78c9328aefd45
realjbtytm <businessjbtytm@gmail.com>
2017-12-25 10:49:03 -0500

    ERROR: Unexpected identitfier in bot.js for jsfiles and props.

--- bot.js
+++ bot.js
@@ -1,10 +1,31 @@
 const botSettings = require("./botsettings.json");
 const Discord = require("discord.js");
+const fs = require("fs");
 
 const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir()("./Commands/", (err, files) => {
+    if (err) console.error(err);
+
+    Let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if (jsfiles.length <= 0) {
+        console.log("No commands to be loaded!")
+        return;
+    }
+
+    console.log('Loading ${jsfiles.length} commands!')
+
+    jsfiles.forEach((f, i) => {
+        Let props = require('./commands/${f}');
+        console.log('${i + 1}: ${f} loaded!');
+        bot.commands.set(f, props);
+    });
+
+});
 
 bot.on("ready", async () => {
-    console.log("Drift is at your service. ${bot.user.username}")
+    console.log('Drift is at your service. ${bot.user.username}')
 
 });
 
