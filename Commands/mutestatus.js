31ffa38ba8e1d09eca97827a0aec2184c8d76c86
realjbtytm <businessjbtytm@gmail.com>
2017-12-26 14:02:14 -0500

    Fixing stuff

--- Commands/mutestatus.js
+++ Commands/mutestatus.js
@@ -14,11 +14,10 @@ module.exports.run = async (bot, message, args) => {
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png")  
     if(!role || !toMute.roles.has(role.id)) return message.channel.send({embed});
 
-    await toMute.addRole(role);
     let embed2 = new Discord.RichEmbed()
         .setTitle("Drift Mute")
         .setThumbnail("https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png")
-        .addField("Mute Status - ", "That user is now muted.")
+        .addField("Mute Status - ", "That user is muted.")
         .addField("Created At -", message.author.createdAt)
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png")
     if(!role || toMute.roles.has(role.id)) return message.channel.send({embed2});
