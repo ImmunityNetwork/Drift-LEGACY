31e622dd6b175d6e2f4d099c7b7e35c1950e5c3f
realjbtytm <businessjbtytm@gmail.com>
2017-12-26 12:33:58 -0500

    Alpha 1.0.0 has been released.

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const botSettings = require("./botsettings.json");
 const Discord = require("discord.js");
 const fs = require("fs");
+const prefix = botSettings.prefix;
 
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
@@ -10,23 +11,44 @@ fs.readdir("./Commands/", (err, files) => {
 
     let jsfiles = files.filter(f => f.split(".").pop() === "js");
     if (jsfiles.length <= 0) {
+        console.log('')
         console.log("No commands to be loaded!")
         return;
     }
 
+    console.log(``)
     console.log(`Loading ${jsfiles.length} commands!`)
 
     jsfiles.forEach((f, i) => {
         let props = require(`./commands/${f}`);
         console.log(`${i + 1}: ${f} loaded!`);
-        bot.commands.set(f, props);
+        bot.commands.set(props.help.name, props);
     });
 
 });
 
-bot.on("ready", async () => {
-    console.log(`Drift is at your service. ${bot.user.username}`)
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
 
+    let messageArray = message.content.split(/\s+/g);
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    let cmd = bot.commands.get(command.slice(prefix.length))
+    if(cmd) cmd.run(bot, message, args);
+})
+
+bot.on("ready", async () => {
+    console.log(``)
+    console.log(`${bot.user.username} is at your service.`)
+    console.log(``)
+    console.log("Ready to begin! Serving in " + bot.channels.length + " channels")
+    console.log(``)
+    console.log(bot.commands)
+    bot.user.setPresence({ status: 'online', game: { name: 'Drift is active.' } })
 });
 
 bot.login(botSettings.token);
