2ed93e440b19e3eee2a2ddb61df4546fc7cce9bb
KryptoCrash <kryptocrashmh@gmail.com>
2017-12-28 08:34:17 -0500

    Ban command Working

--- Commands/ban.js
+++ Commands/ban.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     let modlogs = bot.channels.find('name', 'mod-logs');
     console.log(reason);
 
-    if(message.mentions.users.size < 1) return message.reply("you must mention someone to kick them. (Logic at its finest.)").then(message => message.delete(60000));
+    if(message.mentions.users.size < 1) return message.reply("you must mention someone to ban them. (Logic at its finest.)").then(message => message.delete(60000));
     
     if(!modlogs) {
         try{
@@ -25,27 +25,27 @@ module.exports.run = async (bot, message, args) => {
     }
 
 
-    if(!message.guild.member(user).kickable){
-        return message.reply(`I have no power to kick them from the server at this time.`).then(message => message.delete(60000));
+    if(!message.guild.member(user).bannable){
+        return message.reply(`I have no power to ban them from the server at this time.`).then(message => message.delete(60000));
     } else {
         const embed = new Discord.RichEmbed()
         .setTitle('')
         .setAuthor('Drift Moderation -', message.author.avatarURL)
         .setColor(0x00AE86)
-        .addField('Action - ', 'Kick')
+        .addField('Action - ', 'Ban')
         .addField('User - ', `${user.username}#${user.discriminator}`)
         .addField('Moderator - ', `${message.author.username}#${message.author.discriminator}`)
         .addField('Reason - ', `${reason}`)
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
         message.channel.sendEmbed(embed).then(message => message.delete(60000));
         bot.channels.get(modlogs.id).sendEmbed(embed);
-        message.guild.member(user).sendMessage(`You have been kicked by ${message.author.username}#${message.author.discriminator} due to ${reason}`);
-        message.guild.member(user).kick();
+        message.guild.member(user).sendMessage(`You have been banned by ${message.author.username}#${message.author.discriminator} due to ${reason}`);
+        message.guild.member(user).ban();
     }
 
 }
 
 module.exports.help = {
-    name: "kick"
+    name: "ban"
 }
 
