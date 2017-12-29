ccb3f24125a0403e1fa6526b98eaf6d618143467
KryptoCrash <kryptocrashmh@gmail.com>
2017-12-29 12:20:31 -0500

    Fixed server.js, and added purge command to modlogs.

--- Commands/purge.js
+++ Commands/purge.js
@@ -2,7 +2,18 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let messages = args[0];
-  
+    let delet = messages+1;
+    let modlogs = bot.channels.find('name', 'mod-logs');
+    if(!modlogs) {
+        try{
+            modlogs = await message.guild.createChannel(
+                `mod-logs`,
+                `text`);
+            message.reply("Please set up the permissions for #mod-logs according to your needs manually. Automatic setup of #mod-logs will come shortly. Thanks for your cooperation.")
+        }catch(e){
+            console.log(e.stack);
+        }
+    }
         message.delete();
         const fetched = await message.channel.fetchMessages({limit: args[0]});
         
@@ -12,10 +23,11 @@ module.exports.run = async (bot, message, args) => {
         .setAuthor("Drift Purge")
         .setDescription(`Deleted ${messages} messages in ${message.channel.name}.`)
         .setThumbnail("https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png")
-        .setColor("#9B59B6")
+        .setColor(0x00AE86)
+        .addField('Moderator - ', `${message.author.username}#${message.author.discriminator}`)
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
 message.channel.send({embed: embed}).then(message => message.delete(60000)); 
-
+bot.channels.get(modlogs.id).sendEmbed(embed);
 }
 
 module.exports.help = {
--- Commands/server.js
+++ Commands/server.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    let embed = new Discord.RichEmbed() 
+   let embed = new Discord.RichEmbed() 
         .setAuthor(`${message.guild.name}`)
         .setDescription("This is your Server Information!")
         .setThumbnail(`${message.guild.iconURL}`)
@@ -12,6 +12,7 @@ module.exports.run = async (bot, message, args) => {
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
 
     message.channel.send({embed: embed}).then(message => message.delete(60000)); 
+    
 
 }
 
