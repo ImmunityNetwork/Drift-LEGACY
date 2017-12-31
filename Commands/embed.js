705001e00beb2ec69abebac478f9ac4a12e3c560
realjbtytm <businessjbtytm@gmail.com>
2017-12-30 23:08:24 -0500

    embed command

---
+++ Commands/embed.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, msg, args) => {
+
+  msg.delete();
+  const embed = new Discord.RichEmbed()
+    .addField(`An announcement! - `, args.join(" "))
+    .addField(`Announcement By - `, `${msg.author.username}#${msg.author.discriminator}`)
+    .setColor([114, 137, 218])
+    .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
+  msg.channel.send({embed});
+};
+
+module.exports.help = {
+  name: 'embed',
+};
