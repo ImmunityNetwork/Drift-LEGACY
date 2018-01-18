f0ac675e9ebb75ca4b8e519c5802fed2e5739570
realjbtytm <businessjbtytm@gmail.com>
2018-01-17 20:09:24 -0500

    Embed updated, no longer for use in announcements.

--- Commands/embed.js
+++ Commands/embed.js
@@ -3,8 +3,7 @@ module.exports.run = async (bot, msg, args) => {
 
   msg.delete();
   const embed = new Discord.RichEmbed()
-    .addField(`An announcement! - `, args.join(" "))
-    .addField(`Announcement By - `, `${msg.author.username}#${msg.author.discriminator}`)
+    .addField(`${msg.author.username}#${msg.author.discriminator} - `, args.join(" "))
     .setColor([114, 137, 218])
     .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
   msg.channel.send({embed});
