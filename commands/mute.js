a101e73c7c1ce177e3d3a0b19f6a9d3d693b1811
Jellz <xdjellz@gmail.com>
2018-01-22 17:11:39 +1100

    im not kidding

--- commands/mute.js
+++ commands/mute.js
@@ -39,7 +39,7 @@ module.exports.run = async (bot, message, args) => {
         .setTitle('')
         .setAuthor('Drift Moderation -', message.author.avatarURL)
         .setColor(0x00AE86)
-        .addField('User - ', `${user.username}#${user.discriminator} is already muted!`)
+        .addField('User - ', `${user.tag} is already muted!`)
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
         message.channel.sendEmbed(embed2).then(message => message.delete(3500));
     }else{
@@ -48,9 +48,9 @@ module.exports.run = async (bot, message, args) => {
         .setAuthor('Drift Moderation -', message.author.avatarURL)
         .setColor(0x00AE86)
         .addField('Action - ', 'Mute')
-        .addField('User - ', `${user.username}#${user.discriminator}`)
-        .addField('Moderator - ', `${message.author.username}#${message.author.discriminator}`)
-        .addField('Reason - ', `${reason}`)
+        .addField('User - ', user.tag)
+        .addField('Moderator - ', message.author.tag)
+        .addField('Reason - ', reason)
         .setFooter("Drift is protected under GPL-3.0.", "https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png");
         message.guild.member(user).addRole(muteRole).then(() => {
             message.channel.sendEmbed(embed).then(message => message.delete(3500));
