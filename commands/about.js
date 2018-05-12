3db76472fc7a3009f9a658413b291b51957f1ddd
Spiderlogical <thehelpfulspider@gmail.com>
2018-05-12 12:42:04 -0700

    Updated about command

--- commands/about.js
+++ commands/about.js
@@ -7,9 +7,10 @@ module.exports.run = async (bot, message, args) => {
         .setDescription("Some information about Drift!")
         .setThumbnail("https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png")
         .setColor("#9B59B6")
-        .addField("Drift Version - ", "Alpha 1.0.0")
         .addField("Drift Bot - ", "A multipurpose Bot that is free but comes with limited permissions compared to Drifter, but still still enough for a Discord Server to be satisfied.")
-        .addField("Drifter Bot - ", "A Premium Bot that users pay to use with more permissions and other benefits on the official discord server.")
+        .addField("Drift Version - ", "Alpha 1.0.0")
+        .addField("Server Count -", bot.guilds.size)
+        .addField("Discord.js Version -", "11.3.2")
         .addField("Drift Discord Server - ", "[Click to join](https://discord.gg/JYXwCgV), Stay up-to-date with all the updates to Drift!")
     message.channel.send({embed: embed}).then(message => message.delete(60000));
 }
