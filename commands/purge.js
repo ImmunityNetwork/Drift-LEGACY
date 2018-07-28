070eb3a97bfdd873d3cd81b350cfe8ae32c00ad3
Mona Lisa <thehelpfulspider@gmail.com>
2018-07-27 21:41:21 -0700

    added feedback and suggest, some fixes to purge

--- commands/purge.js
+++ commands/purge.js
@@ -4,8 +4,10 @@ const { RichEmbed } = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     let messages = args[0];
     let delet = messages+1;
-    let modlogs = bot.channels.find('name', 'mod-logs');
-    let managemessagesperm = message.channel.permissionsFor(message.member).hasPermission("MANAGE_MESSAGES");
+    let modlogs = message.guild.channels.find('name', 'mod-logs');
+    let managemessagesperm = message.channel.permissionsFor(message.member).has("MANAGE_MESSAGES");
+    let botmanagemessage = message.guild.me.hasPermission("MANAGE_MESSAGES");
+    if (!botmanagemessage) return message.reply("I dont have permmision to do that. I require the `MANAGE MESSAGES` Permission").then(message => message.delete(60000));
     if(!managemessagesperm) return message.reply("You dont have permmision to do that").then(message => message.delete(60000));
     if(!modlogs) {
         try{
@@ -20,7 +22,10 @@ module.exports.run = async (bot, message, args) => {
         message.delete();
         const fetched = await message.channel.fetchMessages({limit: args[0]});
 
-        message.channel.bulkDelete(fetched);
+        message.channel.bulkDelete(fetched).catch(e => {
+          console.log("It seems that an error has occured! \n" + e.stack);
+          message.channel.send("There was an error! This could have been caused by missing permissions or an error in the code. Contact support for help.");
+        });
 
     let embed = new RichEmbed()
         .setAuthor("Drift Purge")
@@ -28,8 +33,11 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail("https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png")
         .setColor(0x00AE86)
         .addField('Moderator - ', `${message.author.username}#${message.author.discriminator}`);
-message.channel.send({embed: embed}).then(message => message.delete(60000));
-message.guild.channels.get(modlogs.id).sendEmbed(embed);
+message.channel.send({embed: embed}).then(message => message.delete(60000).catch(e => {
+  console.log("It seems that an error has occured! \n" + e.stack);
+  message.channel.send("There was an error! This could have been caused by missing permissions or an error in the code. Contact support for help.");
+}));
+message.guild.channels.get(modlogs.id).send({embed});
 }
 
 module.exports.help = {
