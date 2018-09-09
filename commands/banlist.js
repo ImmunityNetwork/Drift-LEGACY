b586e6f2a0a8e5603377136fed575454d752f67b
Zyphen <undefeatable234@gmail.com>
2018-09-08 21:14:17 -0400

    Added BanList.JS

---
+++ commands/banlist.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const arraySort = require('array-sort');
+const table = require('table');
+
+module.exports.run = async (bot, message, args) => {
+
+    let bans = await message.guild.fetchBans().catch(error => {
+        return message.channel.send('Sorry, I don\'t have the proper permissions to view bans!');
+    });
+
+    bans = bans.array();
+    
+    arraySort(bans, 'size', {
+        reverse: true
+    });
+
+    let possiblebans = bans.map(b => [b.username, b.id])
+    possiblebans.unshift(['Users', 'ID'])
+
+    const embed = new Discord.RichEmbed()
+        .setColor(0xCB5A5E)
+        .addField('Bans', `\`\`\`${table.table(possiblebans)}\`\`\``);
+
+    message.channel.fetchWebhooks().then(webhook => {
+        let banListWH = webhook.find("name", "Drift")
+
+        if(!banListWH) {
+            message.channel.createWebhook("Drift", "https://cdn.discordapp.com/avatars/417450858024796161/0e4870e7a97dff4a12333eb4d2822ddf.png")
+            webhook.send(embed)
+        }
+
+        banListWH.send(embed);
+
+    })    
+};
+
+module.exports.help = {
+    name: "banlist"
+}
