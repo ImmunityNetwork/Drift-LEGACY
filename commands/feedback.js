52c0ef650b80b3f51146f6802dd2a656b66087c0
Freak-san <freakyfreak001@gmail.com>
2018-09-12 15:56:32 -0700

    Feedbak

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,15 +1,14 @@
 const { RichEmbed } = require("discord.js");
-exports.run = (bot, message, args) => {
-  return message.reply("This command is currently disabled while being fixed.");
-  /*if (args.length < 1) return message.channel.send("You need to provide something to say (Abuse of this command may result in punishment)");
-  args = args.join(" ");
+exports.run = async (bot, message, args) => {
+  if (args.length < 1) return message.channel.send("You need to provide something to say (Abuse of this command may result in punishment)");
+  
   // Main Server
 
-  let flog = bot.channels.get("484461023013634059");
+  let flog = bot.channels.find(c => c.id === "484461023013634059");
 
   // Staff Server
 
-  let flog2 = bot.channels.get("486364621650001962");
+  let flog2 = bot.channels.find(c => c.id === "486364621650001962");
 
   const embed = new RichEmbed()
     .setTitle("Feedback")
@@ -17,12 +16,9 @@ exports.run = (bot, message, args) => {
     .setTimestamp()
     .setColor("#1bade2")
     .setDescription(args);
-  flog.send({embed:
-    embed
-  });
-  flog2.send({embed:
-    embed
-  });*/
+  await flog.send(embed)
+
+  await flog2.send(embed)
 };
 
 
