d976d7a269ab16ce7f50548091eae5df11081d9e
KryptoCrash <kryptocrashmh@gmail.com>
2018-09-15 00:44:21 +0000

    New commands

---
+++ commands/lmgtfy.js
@@ -0,0 +1,12 @@
+const encode = require('strict-uri-encode');
+
+module.exports.run = async (bot, message, args) => {
+  let question = encode(args.join(' '));
+  let link = `https://www.lmgtfy.com/?q=${question}`;
+  message.channel.send(`**<${link}>**`);;
+}
+
+module.exports.help = {
+name:"lmgtfy",
+description:"test"
+}
---
+++ commands/skip.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js"); const YTDL = require("ytdl-core");
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if(server.queue[0]) play(connection, messsage);
+        else connection.disconnect();
+    })
+}
+var servers = {}; module.exports.run = (bot, message, args) => {
+    //skip
+    var server = servers[message.guild.id];
+    if (server.dispatcher) server.dispatcher.end();
+}
+module.exports.help = {
+    name: "skip"
+}
