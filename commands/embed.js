1c518092e7192c08770dd8e63293bf770219c58d
Zyphen <undefeatable234@gmail.com>
2018-10-06 23:10:16 -0400

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -4,6 +4,8 @@ module.exports.run = async (bot, msg, args) => {
 
   msg.delete();
   const embed = new RichEmbed()
+    .setAuthor('Drift Miscellaneous -', message.author.avatarURL)
+    .setTitle("Embed")
     .addField(`${msg.author.tag} - `, args.join(" "))
     .setColor("#1bade2");
   msg.channel.send({embed: embed}).catch(e => require('../utils/error.js').error(bot, e));
