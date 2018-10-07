3c1aafa3f061b4a903d59e0db51f4abcdc5d53a9
Zyphen <undefeatable234@gmail.com>
2018-10-06 23:14:06 -0400

    Update launch.js

--- commands/launch.js
+++ commands/launch.js
@@ -4,35 +4,30 @@ const { RichEmbed } = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     console.log(args);
 
-    let user = message.mentions.users.first();
+    let user = message.mentions.users.first() || message.author;
     let randomnumber = Math.floor(Math.random() * 2);
-
-
-
-    if(message.mentions.users.size < 1) return message.reply("You must mention someone to launch them. (Logic at its finest.)").then(message => message.delete(60000));
-
 
 
     if(randomnumber === 1){
     let embed = new RichEmbed()
-        .setTitle('')
-        .setAuthor('Drift Rocket -', 'https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png')
+        .setTitle('Rocket')
+        .setAuthor('Drift Miscellaneous -', message.author.avatarURL)
         .setThumbnail('https://static.pexels.com/photos/73871/rocket-launch-rocket-take-off-nasa-73871.jpeg')
         .setColor("#1bade2")
         .addField('Action - ', 'Launch')
         .addField(user.username + ' was launched into space by ' + message.author.username + '!', user.username + '`s rocket went to space!')
         .setImage('http://bestanimations.com/Sci-Fi/Rockets/nasa-rocket-in-space-animated-gif.gif');
-    message.channel.sendEmbed(embed).then(message => message.delete(60000)).catch(e => require("../utils/error.js").error(bot, e));;
+    message.channel.send(embed).then(message => message.delete(60000)).catch(e => require("../utils/error.js").error(bot, e));;
     } else {
     let embed = new RichEmbed()
-        .setTitle('')
-        .setAuthor('Drift Rocket -', 'https://cdn.discordapp.com/attachments/390285194617421835/394940813865385995/FFADA4B0-4EF6-4441-BAE8-C525975E7418.png')
+        .setTitle('Rocket')
+        .setAuthor('Drift Miscellaneous -', message.author.avatarURL)
         .setThumbnail('https://static.pexels.com/photos/73871/rocket-launch-rocket-take-off-nasa-73871.jpeg')
         .setColor("#1bade2")
         .addField('Action - ', 'Launch')
         .addField(user.username + ' was launched into space by ' + message.author.username + '!', user.username + '`s rocket blew up!')
         .setImage('https://i.imgur.com/8jO42v5.gif');
-    message.channel.sendEmbed(embed).then(message => message.delete(60000)).catch(e => require("../utils/error.js").error(bot, e));;
+    message.channel.send(embed).then(message => message.delete(60000)).catch(e => require("../utils/error.js").error(bot, e));;
 
     }
 
