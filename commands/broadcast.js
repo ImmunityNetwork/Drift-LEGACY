76d93d4c7923bf9c19a6e33f4ccd2d445beec7e0
Zyphen <undefeatable234@gmail.com>
2018-10-25 19:32:28 -0400

    Update broadcast.js

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -3,14 +3,15 @@ module.exports.run = async (bot, message, args) => {
   let br = args.slice(0).join(' ');
   let userid = message.author.id;
 let developers = [
-"236975433176842240",
-"235475763464372224",
-"283739077507809288",
 "203516441683558400",
+"236975433176842240",
 "328580491290607616",
+"322643069730029569",
+"240310996390903808",
+"283739077507809288",
 "483363661285687317"
 ]; 
-  if(developers.includes(userid)==false) return message.channel.send("You do not have access to this command!");
+    if(developers.includes(userid)==false) return message.channel.send("You do not have access to this command!");
  
  let guildList = bot.guilds.array();
         try {
